4240e004b7eb155b3890716e063f2ce709e1a5bb
Bence Agocs <bence@harom.studio>
2021-06-30 22:10:56 +0200

    lakasvalszto fields updated

--- src/wp-theme/page-lakasvalaszto.php
+++ src/wp-theme/page-lakasvalaszto.php
@@ -307,7 +307,7 @@ get_header(); ?>
                                         <div class="data-title">Erkély</div>   
                                         <div class="data-value">
                                             <?php the_field('erkely'); ?>
-                                            <?php if ( get_field('erkely_2') ) { ?> + <?php the_field('erkely_2'); ?>
+                                            <?php if ( get_field('erkely_2') ) { ?> + <?php the_field('erkely_2'); }?>
                                             m²</div>
                                     </div>
                                 <?php } ?>
@@ -316,7 +316,7 @@ get_header(); ?>
                                         <div class="data-title">Terasz</div>   
                                         <div class="data-value">
                                             <?php the_field('terasz'); ?>
-                                            <?php if ( get_field('terasz_2') ) { ?> + <?php the_field('terasz_2'); ?>
+                                            <?php if ( get_field('terasz_2') ) { ?> + <?php the_field('terasz_2'); } ?>
                                             m²</div>
                                     </div>
                                 <?php } ?>
