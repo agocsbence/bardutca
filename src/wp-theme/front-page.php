9b5044ef7955948dfcf1f7861de946e6d9331457
Bence Agocs <bence@harom.studio>
2021-06-30 14:52:35 +0200

    front page fields added

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -11,14 +11,14 @@ get_header();
 
 
 
-<section class="section section--hero bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/header-bg.png');">
+<section class="section section--hero bg--image" style="background-image: url('<?php the_field('hero_image'); ?>');">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-8 offset-md-2 text-center p-4">
 				<div id="hero-text" class="p-4">
-					<h3>Hallottál már ilyet?</h3>
-					<h3>A rezsidet 3 évig mi fizetjük helyetted!*</h3>
-					<div class="btn btn-primary mt-4">Megnézem</div>
+					<h3><?php the_field('hero_text_1'); ?></h3>
+					<h3><?php the_field('hero_text_2'); ?></h3>
+					<a href="<?php the_field('hero_link'); ?>" class="btn btn-primary mt-4">Megnézem</a>
 				</div>
 			</div>
 		</div>
@@ -29,19 +29,19 @@ get_header();
 		<div class="row justify-content-center text-center p-4">
 			<div class="col-6 col-lg-3">
 				<img src="<?php bloginfo('template_url') ?>/assets/img/icon-earth.png" alt="">
-				<h3>Megújuló energia</h3>
+				<h3><?php the_field('icon_text_1'); ?></h3>
 			</div>
 			<div class="col-6 col-lg-3">
 				<img src="<?php bloginfo('template_url') ?>/assets/img/icon-home.png" alt="">
-				<h3>27db lakás</h3>
+				<h3><?php the_field('icon_text_2'); ?></h3>
 			</div>
 			<div class="col-6 col-lg-3">
 				<img src="<?php bloginfo('template_url') ?>/assets/img/icon-ruler.png" alt="">
-				<h3>30,8㎡-130,5㎡</h3>
+				<h3><?php the_field('icon_text_3'); ?></h3>
 			</div>
 			<div class="col-6 col-lg-3">
 				<img src="<?php bloginfo('template_url') ?>/assets/img/icon-bulb.png" alt="">
-				<h3>Okos otthonok</h3>
+				<h3><?php the_field('icon_text_4'); ?></h3>
 			</div>
 		</div>
 	</div>
@@ -50,13 +50,10 @@ get_header();
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-lg-6 order-lg-2">
-				<img src="https://bardutca.hu/wp-content/uploads/2020/03/Ba_CC_81rd-1920-_E2_80_93-te_CC_81rke_CC_81p_50f13628b3fc53a42dd47030fed76968.jpg.webp" alt="" class="img-fluid">
+				<img src="<?php the_field('block_1_image'); ?>" alt="" class="img-fluid">
 			</div>
 			<div class="col-12 col-lg-6 order-lg-1">
-				<h2>Újépítésű, hőszivattyús lakások <br>A megújuló millennium negyedben</h2>
-				<p>Budapest IX. kerületében, Bárd utca 3. szám alatt épülő, kiváló elosztással bíró, fiatalos lakásaink azokat várják, akik családias, meghitt környezetre vágynak, ám mégsem fosztanák meg magukat a belváros nyújtotta előnyöktől.</p>
-				<p>Ferencváros szívében, frekventált, rehabilitált, dinamikusan fejlődő környezetben kerül kivitelezésre. Hitvallásunk szerint a legmodernebb eszközökkel építünk, a modern emberek elvárásainak megfelelően. A kis lakásszám számtalan előnnyel bír.</p>
-				<p>Rohanj az éjszakába, vagy éppen reggel dolgozni, hisz a Bárd utca a főváros ütőerén fekszik, és percek alatt a szívéhez juttat. Intézd el ügyeid a plázában, vagy kapcsolódj ki a Duna-parton, a te vágyad, a te választásod.</p>
+			<?php the_field('block_1_text'); ?>
 			</div>
 		</div>
 	</div>
@@ -65,14 +62,10 @@ get_header();
 	<div class="container bg--image text-white p-4" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/bg.jpg');">
 		<div class="row p-2">
 			<div class="col-12 col-lg-6">
-				<img src="<?php bloginfo('template_url') ?>/assets/img/epitkezes-allasa.jpg" alt="" class="img-fluid">
+				<img src="<?php the_field('block_2_image'); ?>" alt="" class="img-fluid">
 			</div>
 			<div class="col-12 col-lg-6 pl-2">
-				<h2>Ilyet még nem hallottál az biztos!</h2>
-				<p><strong>Ultramodern újépítésű lakásodban – nem számít mekkora – mi fizetjük helyetted a rezsit*. </strong></p>
-				<p>Reméljük erre felkaptad a fejed. Nincs ebben azonban semmi csoda, nem ment el az eszünk. Bebizonyítjuk, hogy az általunk tervezett és kivitelezett épület olyan minőséget képvisel, amire Te és mi is hosszútávon büszkék lehetünk.</p>
-				<p>Modern gépészetének és szigetelésének köszönhetően minimális fenntartási költségekkel kell számolniuk az itt lakást vásárlóknak. Magas minőség elérhető áron – Ez a Bárd3!</p>
-				<p><small>*A hőszivattyús fűtés, hűtés és melegvíz előállítás költségét fizetjük neked a lakás méretétől függetlenül 36 hónapos időszakra. Az akció más ajánlattal össze nem vonható és nem minősül ajánlattételnek.</small></p>
+			<?php the_field('block_2_text'); ?>
 			</div>
 		</div>
 	</div>
@@ -81,15 +74,10 @@ get_header();
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-lg-6 order-lg-2">
-				<img src="<?php bloginfo('template_url') ?>/assets/img/garancia.jpg" alt="" class="img-fluid">
+				<img src="<?php the_field('block_3_image'); ?>" alt="" class="img-fluid">
 			</div>
 			<div class="col-12 col-lg-6 order-lg-1">
-				<h2>Biztonság és garancia</h2>
-				<p>100% pénz-visszafizetési garanciával</p>
-				<p>Számunkra a legfontosabb a vevőink bizalma. Elsődleges célunk, hogy minden a megállapodásunknak és szerződésünknek megfelelően kerüljön átadásra.</p>
-				<p>A Beruházó társaság többezer lakást épített már az elmúlt évtizedekben. A vállalat főprofilja társasházak, luxus villák építése. Most is a legmodernebb műszaki megoldásokkal, igazi értéket teremtünk.</p>
-				<h2>Mindezt banki garanciával!</h2>
-				<p>Fontos számunkra vevőink bizalma, hosszú távra tervezünk még az ingatlanpiacon.</p>
+				<?php the_field('block_3_text'); ?>
 			</div>
 		</div>
 	</div>
@@ -101,13 +89,16 @@ get_header();
 				<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-						<img src="<?php bloginfo('template_url') ?>/assets/img/slider-1.jpg" class="d-block w-100" alt="...">
+							<img src="<?php the_field('slider_1'); ?>" class="d-block w-100" alt="...">
 						</div>
 						<div class="carousel-item">
-						<img src="<?php bloginfo('template_url') ?>/assets/img/slider-1.jpg" class="d-block w-100" alt="...">
+							<img src="<?php the_field('slider_2'); ?>" class="d-block w-100" alt="...">
 						</div>
 						<div class="carousel-item">
-						<img src="<?php bloginfo('template_url') ?>/assets/img/slider-1.jpg" class="d-block w-100" alt="...">
+							<img src="<?php the_field('slider_3'); ?>" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img src="<?php the_field('slider_4'); ?>" class="d-block w-100" alt="...">
 						</div>
 					</div>
 					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -122,8 +113,7 @@ get_header();
 			</div>
 			<div class="col-12 col-lg-5">
 				<div class="p-5">
-					<h2>Készítettünk nektek 4 különböző enteriőrt.</h2>
-					<p>Nézzétek meg. Ha tetszik kérd így, ha nem akkor változtass bármit kedved szerint.</p>
+					<?php the_field('slider_text'); ?>
 				</div>
 			</div>
 		</div>
@@ -131,58 +121,53 @@ get_header();
 </section>
 <section class="section mt-5">
 	<div class="container text-center">
-		<h2>Szolgáltatásaink - Minden egy helyen!</h2>
-		<p>Profi csapatunk segít benne, hogy akár befektetési célra vásárol, akár saját használatra, minden gördülékeny legyen az elejétől a végéig.</p>
+		<?php the_field('services_text'); ?>
 		<div class="row">
 			<div class="col-12 col-lg-3">
-				<div class="btn btn-secondary">Lakberendezés</div>
+				<div class="btn btn-secondary"><?php the_field('service_1'); ?></div>
 			</div>
 			<div class="col-12 col-lg-3">
-				<div class="btn btn-secondary">Kiadás</div>
+				<div class="btn btn-secondary"><?php the_field('service_2'); ?></div>
 			</div>
 			<div class="col-12 col-lg-3">
-				<div class="btn btn-secondary">Ingatlankezelés</div>
+				<div class="btn btn-secondary"><?php the_field('service_3'); ?></div>
 			</div>
 			<div class="col-12 col-lg-3">
-				<div class="btn btn-secondary">Hitelügyintézés</div>
+				<div class="btn btn-secondary"><?php the_field('service_4'); ?></div>
 			</div>
 		</div>
 	</div>
 </section>
 <section class="section my-5">
 	<div class="container bg--image text-white text-center p-5" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/home-green.jpg');">
-		<h2>Válaszd ki új otthonod!</h2>
-		<p>Praktikus elosztású nappali + 1 hálószobás lakásaink közül válogathatsz, illetve egy 9. emeleti menő penthouse lakás is a tiéd lehet még! Több, mint ezer átadott lakással a hátunk mögött rengeteg tapasztalatot gyűjtöttünk. Mi azt a lakást építjük, amit már évek óta kerestél. Megvan benne minden, amit ekkora négyzetméteren elvárhat az ember. Mindenből csak annyi, amire valóban szükséged van.</p>
-		<a href="" class="btn btn-primary mt-4">Nézd meg a teljes lakáslistát</a>
+		<?php the_field('cta_block_text'); ?>
+		<a href="<?php the_field('cta_block_link'); ?>" class="btn btn-primary mt-4">Nézd meg a teljes lakáslistát</a>
 	</div>
 </section>
 <section class="section my-5">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-lg-6">
-				<img src="<?php bloginfo('template_url') ?>/assets/img/lakas.jpg.webp" alt="" class="img-fluid">
+				<img src="<?php the_field('block_4_image'); ?>" alt="" class="img-fluid">
 			</div>
 			<div class="col-12 col-lg-6">
-				<h2>Itt a nyakunkon 2021. Tudod, ez mit jelent?</h2>
-				<p>„Az Európai Parlament és Tanács 2010/31/EU irányelve 2021. január 1-től valamennyi új épületre kötelezően előírja a „közel nulla” energiafogyasztás elérését.” Ennek eléréséhez a legjobb választás a hőszivattyú.</p>
-				<h2>Tiszta energia</h2>
-				<p>„A hőszivattyús hőenergia termelés olcsó, stabil és maximálisan komfortos működése már hazánkban is évtizedes múltra tekint vissza. Nem véletlen hogy a már elfogadott törvény szerint 2020-tól csak alternatív energiát hasznosító lakóépületre adnak építési engedélyt. Ami egyben azt is jelenti, hogy a korábban alkalmazott technológiák várhatóan már a közeli jövőben elavultnak, így nehezen hasznosíthatónak fognak számítani.”</p>
+				<?php the_field('block_4_text'); ?>
 			</div>
 		</div>
 	</div>
 </section>
 <section class="section">
 	<div class="container mb-5">
-		<h2 class="text-center mx-5 mb-5">Lakásainkba az elérhető legmodernebb Mitsubishi hőszivattyúkat szereltük, melyekről az alábbi videókat javasoljuk megtekinteni:</h2>
+		<h2 class="text-center mx-5 mb-5"><?php the_field('video_text'); ?></h2>
 		<div class="row justify-content-center">
 			<div class="col-12 col-lg-4">
-				<iframe src="https://www.youtube.com/embed/tjOQB0plssM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
+				<iframe src="https://www.youtube.com/embed/<?php the_field('video_id_1'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
 			</div>
 			<div class="col-12 col-lg-4">
-				<iframe src="https://www.youtube.com/embed/tjOQB0plssM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
+				<iframe src="https://www.youtube.com/embed/<?php the_field('video_id_2'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
 			</div>
 			<div class="col-12 col-lg-4">
-				<iframe src="https://www.youtube.com/embed/tjOQB0plssM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
+				<iframe src="https://www.youtube.com/embed/<?php the_field('video_id_3'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="img-fluid"></iframe>
 			</div>
 		</div>
 	</div>
@@ -192,23 +177,23 @@ get_header();
 		<div class="row">
 			<div class="col-12 col-lg-4">
 				<div class="testimonial">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/client.png" alt="">
-					<p>Igazi otthont kaptam. Minden esetben kétséget, bizonytalanságot eloszlatva adtak választ kérdéseimre és igyekeztek maradéktalanul eleget tenni a menet közben felmerülő kéréseimnek is. Mindenkinek ilyen biztos hátteret kívánok, ha lakásvásárlásra adja a fejét!</p>
-					<p class="lead">Gábor</p>
+					<img src="<?php the_field('testimonial_image_1'); ?>" alt="">
+					<p><?php the_field('testimonial_text_1'); ?></p>
+					<p class="lead"><?php the_field('testimonial_name_1'); ?></p>
 				</div>
 			</div>
 			<div class="col-12 col-lg-4">
 				<div class="testimonial">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/client.png" alt="">
-					<p>Igazi otthont kaptam. Minden esetben kétséget, bizonytalanságot eloszlatva adtak választ kérdéseimre és igyekeztek maradéktalanul eleget tenni a menet közben felmerülő kéréseimnek is. Mindenkinek ilyen biztos hátteret kívánok, ha lakásvásárlásra adja a fejét!</p>
-					<p class="lead">Gábor</p>
+					<img src="<?php the_field('testimonial_image_2'); ?>" alt="">
+					<p><?php the_field('testimonial_text_2'); ?></p>
+					<p class="lead"><?php the_field('testimonial_name_2'); ?></p>
 				</div>
 			</div>
 			<div class="col-12 col-lg-4">
 				<div class="testimonial">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/client.png" alt="">
-					<p>Igazi otthont kaptam. Minden esetben kétséget, bizonytalanságot eloszlatva adtak választ kérdéseimre és igyekeztek maradéktalanul eleget tenni a menet közben felmerülő kéréseimnek is. Mindenkinek ilyen biztos hátteret kívánok, ha lakásvásárlásra adja a fejét!</p>
-					<p class="lead">Gábor</p>
+					<img src="<?php the_field('testimonial_image_3'); ?>" alt="">
+					<p><?php the_field('testimonial_text_3'); ?></p>
+					<p class="lead"><?php the_field('testimonial_name_3'); ?></p>
 				</div>
 			</div>
 		</div>
@@ -219,10 +204,10 @@ get_header();
 		<div class="row">
 			<div class="col-12 col-lg-6">
 				<h2>Kapcsolat</h2>
-				<p class="lead">Alabárd Invest Kft.</p>
-				<p><span>Cím: </span>9. kerület, Bárd utca 3.</p>
-				<p><span>Mobil: </span><a class="nolinkdecor" href="tel:+36204366000">+36 20 436 6000</a></p>
-				<p><span>Email: </span><a class="nolinkdecor" href="mailto:ertekesites@bardutca.hu">ertekesites@bardutca.hu</a></p>
+				<p class="lead"><?php the_filed('cegnev'); ?></p>
+				<p><span>Cím: </span><?php the_filed('cim'); ?></p>
+				<p><span>Mobil: </span><a class="nolinkdecor" href="tel:<?php the_filed('mobil'); ?>"><?php the_filed('mobil'); ?></a></p>
+				<p><span>Email: </span><a class="nolinkdecor" href="mailto:<?php the_filed('email'); ?>"><?php the_filed('email'); ?></a></p>
 			</div>
 			<div class="col-12 col-lg-6">
 				<form action="">
