3b2589abf77e60a4a0d77094cf880a0232e2fd65
Bence Agocs <bence@harom.studio>
2021-07-08 07:23:21 +0200

    px-0 classes added

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -72,7 +72,7 @@ get_header();
 </section>
 <section class="section my-5">
 	<div class="container px-0">
-		<div class="row">
+		<div class="row align-items-center">
 			<div class="col-12 col-lg-6 order-lg-2">
 				<img src="<?php the_field('block_3_image', 1567); ?>" alt="" class="img-fluid">
 			</div>
@@ -146,7 +146,7 @@ get_header();
 </section>
 <section class="section my-5">
 	<div class="container px-0">
-		<div class="row">
+		<div class="row align-items-center">
 			<div class="col-12 col-lg-6">
 				<img src="<?php the_field('block_4_image', 1567); ?>" alt="" class="img-fluid">
 			</div>
