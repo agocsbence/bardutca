d8970ec4c40d4c5ba4ae7686e2729cd7dad9b9ea
Bence Agocs <bence@harom.studio>
2021-07-26 22:50:40 +0200

    main page url updated

--- dist/wp-theme/page-galeria.php
+++ dist/wp-theme/page-galeria.php
@@ -35,13 +35,116 @@ get_header(); ?>
                 </div>
             </div>
 
+        <?php endwhile; endif; ?>   
+        <?php
+        if( have_rows('galeria_2') ): while ( have_rows('galeria_2') ) : the_row(); ?>
+
+            <div class="col-sm-12 col-lg-6">
+                <div class="swiper-container mySwiper" id="galeria_2">
+                    <div class="swiper-wrapper">
+
+                        <?php if( have_rows('kepek') ): while ( have_rows('kepek') ) : the_row(); ?>
+                            <div class="swiper-slide">
+                                <a href="<?php echo get_sub_field('kep'); ?>"><img src="<?php echo get_sub_field('kep'); ?>" alt=""></a>
+                            </div>
+                        <?php endwhile; endif; ?>
+
+                    </div>
+
+                    <div class="swiper-title">
+                        <h2><?php the_sub_field('galeria_neve') ?></h2>
+                    </div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="swiper-pagination swiper-pagination-white"></div>
+                </div>
+            </div>
+
         <?php endwhile; endif; ?>    
 
     </div>
 </div>
 <div class="container">
     <div class="row">
-        
+        <?php
+        if( have_rows('galeria_3') ): while ( have_rows('galeria_3') ) : the_row(); ?>
+
+            <div class="col-sm-12 col-lg-6">
+                <div class="swiper-container mySwiper" id="galeria_3">
+                    <div class="swiper-wrapper">
+
+                        <?php if( have_rows('kepek') ): while ( have_rows('kepek') ) : the_row(); ?>
+                            <div class="swiper-slide">
+                                <a href="<?php echo get_sub_field('kep'); ?>"><img src="<?php echo get_sub_field('kep'); ?>" alt=""></a>
+                            </div>
+                        <?php endwhile; endif; ?>
+
+                    </div>
+
+                    <div class="swiper-title">
+                        <h2><?php the_sub_field('galeria_neve') ?></h2>
+                    </div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="swiper-pagination swiper-pagination-white"></div>
+                </div>
+            </div>
+
+        <?php endwhile; endif; ?>   
+        <?php
+        if( have_rows('galeria_4') ): while ( have_rows('galeria_4') ) : the_row(); ?>
+
+            <div class="col-sm-12 col-lg-6">
+                <div class="swiper-container mySwiper" id="galeria_4">
+                    <div class="swiper-wrapper">
+
+                        <?php if( have_rows('kepek') ): while ( have_rows('kepek') ) : the_row(); ?>
+                            <div class="swiper-slide">
+                                <a href="<?php echo get_sub_field('kep'); ?>"><img src="<?php echo get_sub_field('kep'); ?>" alt=""></a>
+                            </div>
+                        <?php endwhile; endif; ?>
+
+                    </div>
+
+                    <div class="swiper-title">
+                        <h2><?php the_sub_field('galeria_neve') ?></h2>
+                    </div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="swiper-pagination swiper-pagination-white"></div>
+                </div>
+            </div>
+
+        <?php endwhile; endif; ?>   
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <?php
+        if( have_rows('galeria_5') ): while ( have_rows('galeria_5') ) : the_row(); ?>
+
+            <div class="col-sm-12 col-lg-6">
+                <div class="swiper-container mySwiper" id="galeria_5">
+                    <div class="swiper-wrapper">
+
+                        <?php if( have_rows('kepek') ): while ( have_rows('kepek') ) : the_row(); ?>
+                            <div class="swiper-slide">
+                                <a href="<?php echo get_sub_field('kep'); ?>"><img src="<?php echo get_sub_field('kep'); ?>" alt=""></a>
+                            </div>
+                        <?php endwhile; endif; ?>
+
+                    </div>
+
+                    <div class="swiper-title">
+                        <h2><?php the_sub_field('galeria_neve') ?></h2>
+                    </div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="swiper-pagination swiper-pagination-white"></div>
+                </div>
+            </div>
+
+        <?php endwhile; endif; ?>   
     </div>
 </div>
 
@@ -49,6 +152,62 @@ get_header(); ?>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script>
 var swiper = new Swiper("#galeria_1", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+var swiper2 = new Swiper("#galeria_2", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+var swiper3 = new Swiper("#galeria_3", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+var swiper4 = new Swiper("#galeria_4", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+var swiper5 = new Swiper("#galeria_5", {
     pagination: {
         el: ".swiper-pagination",
     },
@@ -70,6 +229,50 @@ var swiper = new Swiper("#galeria_1", {
 <script type="text/javascript">
 $(document).ready(function() {
     $('#galeria_1 .swiper-wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+    $('#galeria_2 .swiper-wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+    $('#galeria_3 .swiper-wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+    $('#galeria_4 .swiper-wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+    $('#galeria_5 .swiper-wrapper').magnificPopup({
         delegate: 'a',
         type: 'image',
         tLoading: 'Loading image #%curr%...',
